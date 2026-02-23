5d0539697f7022e77a22ca565059a3f0c20f2196
A-Ant Sultan Rahmanya <a-ant.sultan.n4m@id.nssol.nipponsteel.com>
2026-02-23 09:52:56 +0700

    Transfer/Account, add jspecify NonNull for typesafe data in repository

--- src/main/java/id/task/gtech/repository/AccountRepository.java
+++ src/main/java/id/task/gtech/repository/AccountRepository.java
@@ -1,10 +1,11 @@
 package id.task.gtech.repository;
 
 import id.task.gtech.model.Account;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account, String> {
+public interface AccountRepository extends JpaRepository<@NonNull Account, @NonNull String> {
 
 }
--- src/main/java/id/task/gtech/repository/TransferRepository.java
+++ src/main/java/id/task/gtech/repository/TransferRepository.java
@@ -1,9 +1,10 @@
 package id.task.gtech.repository;
 
 import id.task.gtech.model.Transfer;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransferRepository extends JpaRepository<Transfer, Long> {
+public interface TransferRepository extends JpaRepository<@NonNull Transfer, @NonNull Long> {
 }
